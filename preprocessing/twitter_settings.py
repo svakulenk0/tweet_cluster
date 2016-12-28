2bb6d52b3f8cd97c2084dba7b3ae5bec6a6dca79
SV <sv@SVs-MacBook-Air.local>
2016-12-28 20:40:52 +0100

    new: template file for storing Twitter API access credentials

---
+++ preprocessing/twitter_settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Dec 28, 2016
+.. codeauthor: svitlana vakulenko
+<svitlana.vakulenko@gmail.com>
+
+Insert your Twitter API credentials here
+'''
+
+APP_KEY = 'REPLACE_ME'
+APP_SECRET = 'REPLACE_ME'
+OAUTH_TOKEN = 'REPLACE_ME'
+OAUTH_TOKEN_SECRET = 'REPLACE_ME'
+
